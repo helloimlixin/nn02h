48aca645cd7230400da36fd1200c1dd213777e4d
helloimlixin <helloimlixin@gmail.com>
2025-02-01 00:09:45 -0500

    lora finetune

--- finetuning/lora/lora_finetune.py
+++ finetuning/lora/lora_finetune.py
@@ -83,7 +83,7 @@ logger = CSVLogger("logs", name="imdb-sentiment")
 
 # define the trainer
 trainer = pl.Trainer(
-    max_epochs=50,
+    max_epochs=100,
     callbacks=callbacks,
     accelerator="gpu",
     devices=1,
